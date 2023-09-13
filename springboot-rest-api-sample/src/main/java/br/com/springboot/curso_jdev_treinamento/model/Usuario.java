e9c49749a4eec7824d8ed3978283505ab8ded7f9
calebe werneck couto <calebewerneck@hotmail.com>
2023-09-13 10:06:08 -0300

    salvar

---
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/model/Usuario.java
@@ -0,0 +1,93 @@
+package br.com.springboot.curso_jdev_treinamento.model;
+
+import java.awt.Image;
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario", allocationSize = 1, initialValue = 1)
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
+	private Long id;
+
+	private String nome;
+
+	private int idade;
+
+	private String altura;
+
+	private String peso;
+
+	private String resultadoIMC;
+
+	private String descricao;
+
+	
+
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getResultadoIMC() {
+		return resultadoIMC;
+	}
+
+	public void setResultadoIMC(String resultadoIMC) {
+		this.resultadoIMC = resultadoIMC;
+	}
+
+	public String getAltura() {
+		return altura;
+	}
+
+	public void setAltura(String altura) {
+		this.altura = altura;
+	}
+
+	public String getPeso() {
+		return peso;
+	}
+
+	public void setPeso(String peso) {
+		this.peso = peso;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
